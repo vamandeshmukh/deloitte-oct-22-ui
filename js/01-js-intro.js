1d8713f90d30966a29d1591302ceb368b5b96d40
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-10-20 11:37:29 +0530

    JavaScript

--- js/01-js-intro.js
+++ js/01-js-intro.js
@@ -5,8 +5,16 @@
 console.log("JavaScript Introduction");
 
 
+// old JS function - old style 
 function fun () {
     console.log("fun function invoked.");
 }
 
 fun();
+
+// Modern JS function - prefer this style 
+const fun2 = () => {
+    console.log("fun2 function invoked.")
+}
+
+fun2();
