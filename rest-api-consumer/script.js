0af305397840c8193bcbcb97f7787c5c83cf12fe
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-10-20 16:50:42 +0530

    JavaScript

---
+++ rest-api-consumer/script.js
@@ -0,0 +1,21 @@
+
+const url = 'http://localhost:8082/emp/get-all-emps';
+
+const getAllEmps = () => {
+    console.log('getAllEmps');
+    fetch(url)
+        .then((data) => data.json())
+        .then((json) => {
+            console.log(json);
+            document.getElementById('empList').innerHTML = json;
+        })
+        .catch((error) => {
+            console.log(error.message);
+        });
+
+    // )
+    // .catch((error) => {
+    //     console.log(error.message);
+    // }
+    // );
+};
