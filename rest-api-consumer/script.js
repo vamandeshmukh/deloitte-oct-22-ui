fe22c73a5aee902a127fec796941cfcf793c7d0d
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-10-21 11:47:23 +0530

    consume rest

--- rest-api-consumer/script.js
+++ rest-api-consumer/script.js
@@ -1,18 +1,22 @@
 
 const restApi = `http://localhost:8082/emp`;
 
-const getAllEmps = () => {
-    console.log(`getAllEmps`);
-    fetch(`${restApi}/get-all-emps`)
-        .then(data => data.json())
-        .then((json) => {
-            console.log(json);
-            response = JSON.stringify(json);
-            console.log(response);
-            document.getElementById("empList").innerHTML = response;
+const getEmpById = (empId) => {
+    console.log(`getEmpById`);
+    fetch(`${restApi}/get-emp-by-id/${empId}`, { method: 'GET' })
+        .then(data => {
+            return data.json();
         })
-        .catch(() => {
-            document.getElementById("empList").innerHTML = 'Something is wrong!';
+        .then((response) => {
+            document.getElementById("empData").innerHTML = JSON.stringify(response);
+            document.getElementById("eid").value = '';
+            console.log(JSON.stringify(response));
+
+        })
+        .catch((error) => {
+            document.getElementById("eid").value = '';
+            document.getElementById("empData").innerHTML = 'Something is wrong!';
+            console.log(error);
         });
 }
 
@@ -48,3 +52,54 @@ const addEmp = () => {
         });
 }
 
+const updateEmp = () => {
+    console.log(`updateEmp`);
+    fetch(`${restApi}/update-emp`, {
+        body: JSON.stringify({
+            employeeId: document.getElementById("updateEmployeeId").value,
+            firstName: document.getElementById("updateFirstName").value,
+            salary: document.getElementById("updateSalary").value,
+            department: {
+                departmentId: document.getElementById("updateDepartmentId").value
+            }
+        }),
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(data => data.json())
+        .then((json) => {
+            response = JSON.stringify(json);
+            console.log(response);
+            document.getElementById("empUpdated").innerHTML = response;
+            document.getElementById("updateEmployeeId").value = '';
+            document.getElementById("updateFirstName").value = '';
+            document.getElementById("updateSalary").value = '';
+            document.getElementById("updateDepartmentId").value = '';
+        })
+        .catch((error) => {
+            console.log(error);
+            document.getElementById("empAdded").innerHTML = 'Something is wrong!';
+            document.getElementById("updateEmployeeId").value = '';
+            document.getElementById("updateFirstName").value = '';
+            document.getElementById("updateSalary").value = '';
+            document.getElementById("updateDepartmentId").value = '';
+
+        });
+}
+
+const getAllEmps = () => {
+    console.log(`getAllEmps`);
+    fetch(`${restApi}/get-all-emps`)
+        .then(data => data.json())
+        .then((json) => {
+            console.log(json);
+            response = JSON.stringify(json);
+            console.log(response);
+            document.getElementById("empList").innerHTML = response;
+        })
+        .catch(() => {
+            document.getElementById("empList").innerHTML = 'Something is wrong!';
+        });
+}
